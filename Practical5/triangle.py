5cf03e7e2140de8404f46798451d1f73e46434f2
Match <sisi.21@intl.zju.edu.cn>
2022-03-16 14:58:18 +0800

    triangle.py

---
+++ Practical5/triangle.py
@@ -0,0 +1,10 @@
+#When the number of rows is less than or equal to 10
+#calculat the sum of the numbers from 1 to the number of rows
+#out put the sum
+#Increase one number of rows
+#repeat the above steps
+sum = 0
+for i in range(1,11):
+   sum = sum + i
+   print(i,"row:",sum)
+
