ab79b24425263826bd3f9d9e7b056b6746d9ad6d
Match <sisi.21@intl.zju.edu.cn>
2022-03-16 15:05:19 +0800

    variables.py

---
+++ Practical5/variables.py
@@ -0,0 +1,16 @@
+#3.1
+a = 19245301 
+b = 4218520
+c = 271
+d = b-c
+e = a-b
+print ("d>e");print (d>e)
+#if print true than the rate of new cases in 2020 is greater
+#if print false that the rate of new cases in 2021 is greater
+#3.2
+X = True
+Y = False
+W = X and Y
+
+
+
