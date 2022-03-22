ddc15c978d873c0706dd28b3815cd478e80c0e20
Match <sisi.21@intl.zju.edu.cn>
2022-03-22 15:01:06 +0800

    pizza_party.py2

--- Practical5/pizza_party.py
+++ Practical5/pizza_party.py
@@ -1,11 +1,14 @@
-#Start with one cut and increase one cut each time  
-#calculate the maximum number of pieces of cake that can be cut.  
-#Stop until the maximum number of pieces is greater than or equal to 64
 n=1
+#Start with one cut
 p=0
-while p < 64:
+#p stores the maximum number of pieces of pizza that can be cut
+while p < 64:#Continue cutting when the maximum number of pieces is less than 64
       p=(n**2+n+2)/2
-      print (str(p),"pieces of pizza that can be cut for",str(n),"cut")
+#Calculate the maximum number of pieces that can be cut
+      print (int(p),"pieces of pizza that can be cut for",int(n),"cut")
+#print the maximum number of pieces that can be cut
       n=n+1
+#increase one cut each time
 print ("that is enough for the whole IBI class (64 numbers)")
-
+#Stop until the maximum number of pieces is greater than or equal to 64
+#print that's enough to give IBI a whole class
