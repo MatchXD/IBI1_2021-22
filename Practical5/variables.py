c4caee5eb3fa98f2ebcfaeb0a922568896226352
Match <sisi.21@intl.zju.edu.cn>
2022-03-22 15:02:01 +0800

    variables.py2

--- Practical5/variables.py
+++ Practical5/variables.py
@@ -1,10 +1,17 @@
 #3.1
+#a stores the number of cases in 2022
 a = 19245301 
+#b stores the number of cases in 2021
 b = 4218520
+#c stores the number of cases in 2020
 c = 271
+#d stores the difference between the number of cases in 2020 and 2021
 d = b-c
+#e stores the difference between the number of cases in 2021 and 2022
 e = a-b
+#print the value of d and e
 print ("d =",d,"    e =",e)
+#compare d to e and print it out
 print ("d > e",d>e)
 #if print true that 2020 has the greatest number of new COVID-19 cases
 #if print false that 2021 has the greatest number of new COVID-19 cases
@@ -13,6 +20,7 @@ print ("d > e",d>e)
 X = True
 Y = False
 W = X and Y
+#print the value of W
 print ("W is",W)
 
 
