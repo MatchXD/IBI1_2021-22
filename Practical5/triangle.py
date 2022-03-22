82e0aafcdf7f24917151a5c3bdc8262a78414b44
Match <sisi.21@intl.zju.edu.cn>
2022-03-22 15:01:44 +0800

    triangle.py2

--- Practical5/triangle.py
+++ Practical5/triangle.py
@@ -1,10 +1,13 @@
-#When the number of rows is less than or equal to 10
-#calculat the sum of the numbers from 1 to the number of rows
-#out put the sum
-#Increase one number of rows
-#repeat the above steps
+#Sum is used to store the triangle sequence
 sum = 0
+#When the number of rows is less than or equal to 10,loop continues
+#Increase one number of rows each time
 for i in range(1,11):
+#calculates the triangle sequence in this row
    sum = sum + i
-   print(i,"row:",sum)
-
+#print the triangle sequence corresponding to the current rows after each calculation
+   if i == 1:
+      print(i,"row:",sum)
+#when row==1,print "row"
+#when row>1,print "rows"
+   else: print(i,"rows:",sum)
