b92d21274a74d301a5cbc8c67006059d9a71a383
Match <sisi.21@intl.zju.edu.cn>
2022-05-23 11:42:05 +0800

    Gooo.py

--- Practical14/Gooo.py
+++ Practical14/Gooo.py
@@ -14,20 +14,22 @@ n = 0
 num = []
 num2 = []
 
-def num_go (x):
+
+def num_go(x):
     global dic2
-    for i in range(0,len(dic[x])):
+    for i in range(0, len(dic[x])):
         dic2[dic[x][i]] = 0
         if dic[x][i] in dic:
             num_go(dic[x][i])
     return len(dic2.keys())
+
 
 for term in terms:
     n += 1
     string1 = term.getElementsByTagName('id')[0]
     ID = string1.childNodes[0].data
     number = len(term.getElementsByTagName('is_a'))
-    for i in range(0,number):
+    for i in range(0, number):
         string2 = term.getElementsByTagName('is_a')[i]
         ParentGo = string2.childNodes[0].data
         dic[ParentGo].append(ID)
@@ -42,34 +44,7 @@ for term in terms:
     if 'translation' in string:
         dic2 = {}
         num2.append(num_go(ID))
-'''
-for term in terms:
-    string1 = term.getElementsByTagName('id')[0]
-    ID = string1.childNodes[0].data
-    while ID in dic:
 
-def num_sub (x):
-    global m
-    for i in range(0,len(dic[x])):
-        m += 1
-        sub = dic[x][i]
-        if sub in dic:
-            num_sub(sub)
-
-x = 0
-for term in terms:
-    string1 = term.getElementsByTagName('id')[0]
-    ID = string1.childNodes[0].data
-    while ID in dic:
-        for i in range(0,len(dic[ID])):
-            x += 1
-            ID + str(x)
-
-
-        num.append(m)
-    m = 0
-
-'''
 print('the number of terms is:', n)
 
 plt.boxplot(num, vert=True, whis=1.5, patch_artist=True, meanline=False, showbox=True, showcaps=True, showfliers=True,
@@ -80,11 +55,10 @@ plt.ylabel('number')
 plt.show()
 
 plt.boxplot(num2, vert=True, whis=1.5, patch_artist=True, meanline=False, showbox=True, showcaps=True,
-            showfliers=True,notch=False)
+            showfliers=True, notch=False)
 plt.title('boxplot of the distribution of child nodes across terms associated with translation')
 plt.xlabel('childNodes')
 plt.ylabel('number')
 plt.show()
 
 print('the translation terms contain a smaller number of child nodes than the overall Gene Ontology')
-
