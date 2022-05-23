c909c6352bd002c156196fa263eeeb411831ef1b
Match <sisi.21@intl.zju.edu.cn>
2022-05-23 13:36:00 +0800

    chocolate_bar.py

--- Practical10/chocolate_bar.py
+++ Practical10/chocolate_bar.py
@@ -2,10 +2,13 @@ total_money	= 100
 price =	7
 def cal (x, y, count=0):
     print('totally',x,'money')
-    while x > 0:
+    while x - y > 0:
         x = x - y
-        if x > 0:
-            count += 1
-            print('buy',count,'chocolate bars, remain',x,'money')
-    print('can totally buy',count,'chocolate bars')
+        count += 1
+    print('buy', count, 'chocolate bars, remain', x, 'money')
+
+cal(total_money,price)
+
+total_money = int(input('now, wirte your own total money:'))
+price = int(input('write the price:'))
 cal(total_money,price)
